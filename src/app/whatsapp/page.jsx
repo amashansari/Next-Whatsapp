bdc39a6d9eac38baab993d0df975c19161c21b55
Amash10 <amashansari74@gmail.com>
2025-07-03 14:53:03 +0530

    changes

--- src/app/whatsapp/page.jsx
+++ src/app/whatsapp/page.jsx
@@ -10,21 +10,21 @@ export default function QrDisplay() {
     setLoading(true);
     await fetch('/api/whatsapp/start', { method: 'POST' });
 
-    const poll = setInterval(async () => {
-      const res = await fetch('/api/whatsapp/qr');
-      const data = await res.json();
-      console.log("data", data);
+    // const poll = setInterval(async () => {
+    //   const res = await fetch('/api/whatsapp/qr');
+    //   const data = await res.json();
+    //   console.log("data", data);
 
 
-      if (data.status === 'ready') {
-        clearInterval(poll);
-        setQrImage(data.qrImageUrl);
-        setLoading(false);
-      }
+    //   if (data.status === 'ready') {
+    //     clearInterval(poll);
+    //     setQrImage(data.qrImageUrl);
+    //     setLoading(false);
+    //   }
 
-    }, 2000);
+    // }, 2000);
   };
-  console.log("qrImage", qrImage);
+  // console.log("qrImage", qrImage);
 
 
   return (
@@ -43,7 +43,7 @@ export default function QrDisplay() {
         </div>
       )}
       <div style={{ marginTop: '20px' }}>
-        <img src='/whatsapp-qr.png' alt="WhatsApp QR Code" />
+        <img src='/whatsapp-qr.png' alt="Code" />
       </div>
 
     </div>
